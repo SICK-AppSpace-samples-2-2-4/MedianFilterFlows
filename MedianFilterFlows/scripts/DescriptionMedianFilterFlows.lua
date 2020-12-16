75e3b215e69012da7cdc4620e7500d63936c6611
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 22:55:11 +0100

    Initial commit

---
+++ MedianFilterFlows/scripts/DescriptionMedianFilterFlows.lua
@@ -0,0 +1,19 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name: MedianFilterFlows                                                                                                                       
+                                                                                             
+  Summary:  
+  Scan data from file in resources is retrieved and after applying a median filter, 
+  viewed as a point cloud. This sample uses the flow approach. 
+  
+  Description:                                                                                                                    
+  This sample creates a ScanFileDriver and starts playing of scans from the 
+  test scandata file. The filtered scan is shown in the PointCloud viewer on 
+  the webpage.
+  
+  How to run: 
+  Scans can be viewed in the PointCloud viewer on the webpage.
+  After some time the playing stops and the last scan is still shown. To replay, the 
+  sample must be restarted.
+    
+------------------------------------------------------------------------------]]
---
+++ readme.md
@@ -0,0 +1,13 @@
+## MedianFilterFlows
+Applying a median filter to Scan data retrieved from file. This sample uses the flow approach.
+### Description
+This sample creates a ScanFileDriver and starts playing of scans from the
+test scandata file. The filtered scan is shown in the PointCloud viewer on
+the web-page.
+### How to Run
+Scans can be viewed in the PointCloud viewer on the web-page.
+After some time the playing stops and the last scan is still shown. To replay, the
+sample must be restarted.
+
+### Topics
+Algorithm, Scan, Filtering, Flow, Sample, SICK-AppSpace
